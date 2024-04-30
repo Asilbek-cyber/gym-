8ad46752b58eea3536b650fb868a052f49346d93
muslimbek77 <muslimjonbekali@mail.ru>
2024-04-30 19:45:49 +0500

    contact send message

--- kachka/views.py
+++ kachka/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .models import Trainer
-
+from .models import Trainer,Contact
+from .forms import ContactForm #new
+from .bot import send_message
 def home_view(request):
     trainers = Trainer.objects.all()
     context = {"trainers":trainers}
@@ -11,7 +12,25 @@ def trainer_view(request):
     return render(request, "trainer.html")
 
 def contact_view(request):
-    return render(request, "contact.html")
+    
+    if request.method == "GET":
+        form = ContactForm()
+    else:
+        # contact = Contact.objects.all()
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            phone_number = form.cleaned_data["phone_number"]
+            description = form.cleaned_data["description"]
+
+            send_message(name,email,phone_number,description)
+            form.save()
+            form = ContactForm() 
+             
+    context = {"form":form}
+
+    return render(request, "contact.html",context)
 
 def why_view(request):
     return render(request, "why.html")
